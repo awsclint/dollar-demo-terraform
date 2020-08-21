915af1e7f79af580df64ce5f8d0bfa025fed74a9
White <cliwhite@amazon.com>
2020-08-21 12:46:42 -0400

    broke out VPCs in to folders

--- ecr.tf
+++ ecr.tf
@@ -1,21 +1,31 @@
 resource "aws_ecr_repository" "web" {
-  name                 = "dollars-demo-web"
+  name                 = var.webapp
   image_tag_mutability = "MUTABLE"
   encryption_configuration {
       encryption_type      = "AES256"
   }
   image_scanning_configuration {
     scan_on_push = true
+  }
+  tags               = {
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "cliwhite"
   }
 }
 
-resource "aws_ecr_repository" "app" {
-  name                 = "dollars-demo-app"
+resource "aws_ecr_repository" "api" {
+  name                 = var.api
   image_tag_mutability = "MUTABLE"
   encryption_configuration {
       encryption_type      = "AES256"
   }
   image_scanning_configuration {
     scan_on_push = true
+  }
+  tags               = {
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "cliwhite"
   }
 }
