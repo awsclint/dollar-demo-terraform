ec7832e0ee6f3deeadffb836520e00e3f4d0ec5d
White <cliwhite@amazon.com>
2020-08-24 10:23:00 -0400

    cleaned up roles

--- iam/iam.tf
+++ iam/iam.tf
@@ -1,14 +1,3 @@
-# resource "aws_iam_role" "ecs_service_role" {
-#     name = "ecs_service_role"
-#     assume_role_policy = file("policies/ecs-role.json")
-# }
-
-# resource "aws_iam_role_policy" "ecs_service_role_policy" {
-#     name = "ecs_service_role_policy"
-#     policy = file("policies/ecs-service-role-policy.json")
-#     role = aws_iam_role.ecs_service_role.id
-# }
-
 resource "aws_iam_role" "fargate_execution_role" {
     name = "fargate_execution_role"
     assume_role_policy = file("policies/fargate-execution-role.json")
